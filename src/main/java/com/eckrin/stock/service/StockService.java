39d0584bec6e03bc54087f7da259484406e8aa18
eckrin <bsh51515@naver.com>
2024-01-03 18:33:28 +0900

    240103: race condition 발생 확인

--- src/main/java/com/eckrin/stock/service/StockService.java
+++ src/main/java/com/eckrin/stock/service/StockService.java
@@ -4,6 +4,7 @@ import com.eckrin.stock.domain.Stock;
 import com.eckrin.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +12,7 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
+    @Transactional
     public void decrease(Long id, Long quantity) {
         // Stock 조회, 재고 감소후 갱신값 저장
         Stock stock = stockRepository.findById(id).orElseThrow();
--- src/test/java/com/eckrin/stock/service/StockServiceTest.java
+++ src/test/java/com/eckrin/stock/service/StockServiceTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootTest
 class StockServiceTest {
 
@@ -33,5 +37,28 @@ class StockServiceTest {
 
         Stock ex = stockRepository.findById(1L).orElseThrow();
         Assertions.assertThat(ex.getQuantity()).isEqualTo(99);
+    }
+
+    @Test
+    public void 동시요청() throws InterruptedException {
+        int threadCount = 100;
+        // 쓰레드 32개를 관리하는 쓰레드 풀 객체 생성
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i=0; i<threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown(); // 각 쓰레드의 작업 종료를 명시한다.
+                }
+            });
+        }
+
+        latch.await(); // 메인쓰레드는 latch의 count가 0이 되기를 기다린다.
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        Assertions.assertThat(stock.getQuantity()).isNotEqualTo(0); // race condition으로 인하여 원하는 결과가 나오지 않음
     }
 }
