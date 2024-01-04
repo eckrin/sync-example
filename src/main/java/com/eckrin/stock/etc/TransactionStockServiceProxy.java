bde17f4768458cc4c8dd45e4fe0673720f42b6a8
eckrin <bsh51515@naver.com>
2024-01-05 02:47:14 +0900

    240105: 트랜잭션 프록시 이해

---
+++ src/main/java/com/eckrin/stock/etc/TransactionStockServiceProxy.java
@@ -0,0 +1,28 @@
+package com.eckrin.stock.etc;
+
+import com.eckrin.stock.service.StockService;
+
+public class TransactionStockServiceProxy {
+
+    private StockService stockService;
+
+    public TransactionStockServiceProxy(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long id, Long quantity) {
+        startTransaction();
+
+        stockService.decrease(id, quantity);
+
+        endTransaction();
+    }
+
+    private void startTransaction() {
+        // 트랜잭션 시작
+    }
+
+    private void endTransaction() {
+        // 트랜잭션 커밋
+    }
+}
