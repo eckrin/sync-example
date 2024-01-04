62225ec83031271e6426051ac227861464dd0a4d
eckrin <bsh51515@naver.com>
2024-01-05 02:47:56 +0900

    240105: pessimistic lock을 사용한 해결

--- src/main/java/com/eckrin/stock/repository/StockRepository.java
+++ src/main/java/com/eckrin/stock/repository/StockRepository.java
@@ -1,7 +1,15 @@
 package com.eckrin.stock.repository;
 
 import com.eckrin.stock.domain.Stock;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query(value = "select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(Long id);
+
 }
--- src/test/java/com/eckrin/stock/service/StockServiceTest.java
+++ src/test/java/com/eckrin/stock/service/StockServiceTest.java
@@ -5,6 +5,7 @@ import com.eckrin.stock.repository.StockRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,8 @@ class StockServiceTest {
     private StockService stockService;
     @Autowired
     private StockRepository stockRepository;
+    @Autowired
+    private PessimisticLockStockService pessimisticLockStockService;
 
     @BeforeEach
     public void init() {
@@ -40,6 +43,7 @@ class StockServiceTest {
     }
 
     @Test
+    @DisplayName("synchronized 사용")
     public void 동시요청() throws InterruptedException {
         int threadCount = 100;
         // 쓰레드 32개를 관리하는 쓰레드 풀 객체 생성
@@ -59,6 +63,30 @@ class StockServiceTest {
         latch.await(); // 메인쓰레드는 latch의 count가 0이 되기를 기다린다.
 
         Stock stock = stockRepository.findById(1L).orElseThrow();
-        Assertions.assertThat(stock.getQuantity()).isNotEqualTo(0); // race condition으로 인하여 원하는 결과가 나오지 않음
+        Assertions.assertThat(stock.getQuantity()).isEqualTo(0); // race condition으로 인하여 원하는 결과가 나오지 않읿
+    }
+
+    @Test
+    @DisplayName("pessimistic lock 사용")
+    public void 동시요청_비관락() throws InterruptedException {
+        int threadCount = 100;
+        // 쓰레드 32개를 관리하는 쓰레드 풀 객체 생성
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i=0; i<threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    pessimisticLockStockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown(); // 각 쓰레드의 작업 종료를 명시한다.
+                }
+            });
+        }
+
+        latch.await(); // 메인쓰레드는 latch의 count가 0이 되기를 기다린다.
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        Assertions.assertThat(stock.getQuantity()).isEqualTo(0); // race condition으로 인하여 원하는 결과가 나오지 않읿
     }
 }
